8e74bf406b307da67301c34a1d372b0ffe1e0195
Assaf Urieli <assaf.urieli@gmail.com>
2023-09-15 10:28:15 +0200

    Fix punctuation splitter

--- modules/yiddish/src/main/scala/com/joliciel/jochre/ocr/yiddish/YiddishAltoProcessor.scala
+++ modules/yiddish/src/main/scala/com/joliciel/jochre/ocr/yiddish/YiddishAltoProcessor.scala
@@ -6,8 +6,6 @@ import com.joliciel.jochre.ocr.core.utils.XmlImplicits
 import com.joliciel.jochre.ocr.yiddish.YiddishAltoProcessor.Purpose
 import com.joliciel.yivoTranscriber.YivoTranscriber
 import enumeratum._
-import org.slf4j.LoggerFactory
-import zio._
 
 import java.io.FileInputStream
 import java.util.zip.ZipInputStream
@@ -65,92 +63,123 @@ case class YiddishAltoProcessor(yiddishConfig: YiddishConfig) extends AltoProces
     Set(yivoAlternative, romanizedAlternative).flatten
   }
 
-  private val numberRegex = raw"\d+\.?\d+".r
-  private val punctuationAndNotRegex = raw"(?U)\p{Punct}[^\p{Punct}\d]|[^\p{Punct}\d]\p{Punct}".r
-  private val punctuationRegex = raw"(?U)\p{Punct}".r
-  override def getSpecificRules: Seq[RewriteRule] = Seq(
-    new RewriteRule {
-      override def transform(node: Node): Seq[Node] = node match {
-        case altoString: Elem if altoString.label == "String" =>
-          val content = altoString \@ "CONTENT"
-          val confidence = Option.when(!(altoString \@ "WC").isEmpty)((altoString \@ "WC").toDouble).getOrElse(0)
-          if (punctuationAndNotRegex.findFirstIn(content).isDefined) {
-            // split by punctuation
-            val glyphs = altoString \ "Glyph"
-            val words = glyphs.foldLeft(Seq[Seq[Node]]()){ case (words, glyph) =>
-              if (words.isEmpty) {
-                Seq(Seq(glyph))
-              } else {
-                val currentIsPunct = punctuationRegex.matches(glyph \@ "CONTENT")
-                if (currentIsPunct) {
-                  words :+ Seq(glyph)
-                } else {
-                  val lastGlyph = words.last.map(_.last)
-                  val lastIsPunct = punctuationRegex.matches(lastGlyph \@ "CONTENT")
-                  if (lastIsPunct) {
-                    words :+ Seq(glyph)
-                  } else {
-                    words.init :+ (words.last :+ glyph)
-                  }
-                }
-              }
-            }
-            val wordNodes = words.map { glyphSeq =>
-              val content = glyphSeq.map(glyph => (glyph \@ "CONTENT")).mkString("")
-              val vpos = glyphSeq.map(glyph => (glyph \@ "VPOS")).map(s => if (s.isEmpty) {0} else {Integer.parseInt(s)})
-              val hpos = glyphSeq.map(glyph => (glyph \@ "HPOS")).map(s => if (s.isEmpty) {0} else {Integer.parseInt(s)})
-              val heights = glyphSeq.map(glyph => (glyph \@ "HEIGHT")).map(s => if (s.isEmpty) {0} else {Integer.parseInt(s)})
-              val widths = glyphSeq.map(glyph => (glyph \@ "WIDTH")).map(s => if (s.isEmpty) {0} else {Integer.parseInt(s)})
-
-              val bottom = vpos.zip(heights).map{case (top, height) => top + height }.max
-              val right = hpos.zip(widths).map{case (right, width) => right + width }.max
-              val left = vpos.min
-              val top = hpos.min
-              val height = bottom - top
-              val width = right - left
-
-              val glyphConfidence = glyphSeq.map(glyph => (glyph \@ "GC")).map(s => if (s.isEmpty) {0} else {s.toDouble}).max
-              val myConfidence = if (glyphSeq.size>1) { confidence } else { glyphConfidence }
-              <String VPOS={left.toString} HPOS={top.toString} HEIGHT={height.toString} WIDTH={width.toString} WC={myConfidence.toString} CONTENT={content}></String>
-            }
-            wordNodes
-          } else {
-            altoString
-          }
-        case other => other
-      }
-    },
-    new RewriteRule {
-      override def transform(node: Node): Seq[Node] = node match {
-        case altoString: Elem if altoString.label == "String" =>
-          val content = altoString \@ "CONTENT"
-          if (numberRegex.matches(content)) {
-            val inverseNumber = content.reverse
-            val newAttributes = for (attr <- altoString.attributes) yield attr match {
-              case attr@Attribute("CONTENT", _, _) =>
-                attr.goodCopy (value = inverseNumber)
-              case other => other
-            }
-            altoString.copy(attributes = newAttributes)
-          } else {
-            altoString
-          }
-        case other => other
-      }
-    }
+  override val getSpecificRules: Seq[RewriteRule] = Seq(
+    YiddishAltoProcessor.punctuationSplitRule, YiddishAltoProcessor.reverseNumberRule
   )
 
   override val removeGlyphs: Boolean = true
 }
 
-object YiddishAltoProcessor {
+object YiddishAltoProcessor extends XmlImplicits {
   sealed trait Purpose extends EnumEntry
 
   object Purpose extends Enum[Purpose] {
     val values = findValues
 
     case object YIVO extends Purpose
+
     case object Roman extends Purpose
+  }
+
+  private val punctuationAndNotRegex = raw"(?U)\p{Punct}[^\p{Punct}\d]|[^\p{Punct}\d]\p{Punct}".r
+  private val punctuationRegex = raw"(?U)\p{Punct}".r
+  val punctuationSplitRule = new RewriteRule {
+    override def transform(node: Node): Seq[Node] = node match {
+      case altoString: Elem if altoString.label == "String" =>
+        val content = altoString \@ "CONTENT"
+        val confidence = Option.when(!(altoString \@ "WC").isEmpty)((altoString \@ "WC").toDouble).getOrElse(0)
+        if (punctuationAndNotRegex.findFirstIn(content).isDefined) {
+          // split by punctuation
+          val glyphs = altoString \ "Glyph"
+          val words = glyphs.foldLeft(Seq[Seq[Node]]()) { case (words, glyph) =>
+            if (words.isEmpty) {
+              Seq(Seq(glyph))
+            } else {
+              val currentIsPunct = punctuationRegex.matches(glyph \@ "CONTENT")
+              if (currentIsPunct) {
+                words :+ Seq(glyph)
+              } else {
+                val lastGlyph = words.last.map(_.last)
+                val lastIsPunct = punctuationRegex.matches(lastGlyph \@ "CONTENT")
+                if (lastIsPunct) {
+                  words :+ Seq(glyph)
+                } else {
+                  words.init :+ (words.last :+ glyph)
+                }
+              }
+            }
+          }
+          val wordNodes = words.map { glyphSeq =>
+            val content = glyphSeq.map(glyph => (glyph \@ "CONTENT")).mkString("")
+            val vpos = glyphSeq.map(glyph => (glyph \@ "VPOS")).map(s => if (s.isEmpty) {
+              0
+            } else {
+              Integer.parseInt(s)
+            })
+            val hpos = glyphSeq.map(glyph => (glyph \@ "HPOS")).map(s => if (s.isEmpty) {
+              0
+            } else {
+              Integer.parseInt(s)
+            })
+            val heights = glyphSeq.map(glyph => (glyph \@ "HEIGHT")).map(s => if (s.isEmpty) {
+              0
+            } else {
+              Integer.parseInt(s)
+            })
+            val widths = glyphSeq.map(glyph => (glyph \@ "WIDTH")).map(s => if (s.isEmpty) {
+              0
+            } else {
+              Integer.parseInt(s)
+            })
+
+            val bottom = vpos.zip(heights).map { case (top, height) => top + height }.max
+            val right = hpos.zip(widths).map { case (right, width) => right + width }.max
+            val top = vpos.min
+            val left = hpos.min
+            val height = bottom - top
+            val width = right - left
+
+            val glyphConfidence = glyphSeq.map(glyph => (glyph \@ "GC")).map(s => if (s.isEmpty) {
+              0
+            } else {
+              s.toDouble
+            }).max
+            val myConfidence = if (glyphSeq.size > 1) {
+              confidence
+            } else {
+              glyphConfidence
+            }
+            <String HPOS={left.toString} VPOS={top.toString} WIDTH={width.toString} HEIGHT={height.toString} CONTENT={content} WC={myConfidence.toString}>
+              {glyphSeq}
+            </String>
+          }
+          wordNodes
+        } else {
+          altoString
+        }
+      case other => other
+    }
+  }
+
+  private val numberRegex = raw"\d+\.?\d+".r
+
+  val reverseNumberRule = new RewriteRule {
+    override def transform(node: Node): Seq[Node] = node match {
+      case altoString: Elem if altoString.label == "String" =>
+        val content = altoString \@ "CONTENT"
+        if (numberRegex.matches(content)) {
+          val inverseNumber = content.reverse
+          val newAttributes = for (attr <- altoString.attributes) yield attr match {
+            case attr@Attribute("CONTENT", _, _) =>
+              attr.goodCopy(value = inverseNumber)
+            case other => other
+          }
+          altoString.copy(attributes = newAttributes)
+        } else {
+          altoString
+        }
+      case other => other
+    }
   }
 
   val yiddishConfig: YiddishConfig = YiddishConfig.fromConfig
