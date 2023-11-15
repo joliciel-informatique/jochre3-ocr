933fc0dd5e2bff834bf80bf4dc58e719e45a1895
Assaf Urieli <assaf.urieli@gmail.com>
2023-11-15 15:50:40 +0100

    Fix compile error and ensure we keep spaces

--- modules/yiddish/src/main/scala/com/joliciel/jochre/ocr/yiddish/YiddishTextLineExtractor.scala
+++ modules/yiddish/src/main/scala/com/joliciel/jochre/ocr/yiddish/YiddishTextLineExtractor.scala
@@ -4,7 +4,6 @@ import com.joliciel.jochre.ocr.core.corpus.TextLineExtractor
 
 object YiddishTextLineExtractor {
   def main(args: Array[String]): Unit = {
-    val textLineExtractor = TextLineExtractor(textSimplifier = YiddishTextSimpifier)
-    TextLineExtractor.execute(textLineExtractor, args)
+    TextLineExtractor.execute(args, textSimplifier = YiddishTextSimpifier)
   }
 }
